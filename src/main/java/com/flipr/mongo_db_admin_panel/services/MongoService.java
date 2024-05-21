7b861fe5c1391529e45c6fae4cf8f559a92d98bd
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2024-05-22 00:28:54 +0530

    user can give now dynamic clustur url

--- src/main/java/com/flipr/mongo_db_admin_panel/services/MongoService.java
+++ src/main/java/com/flipr/mongo_db_admin_panel/services/MongoService.java
@@ -1,9 +1,7 @@
 package com.flipr.mongo_db_admin_panel.services;
 
-import com.flipr.mongo_db_admin_panel.models.MongoUser;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Value;
